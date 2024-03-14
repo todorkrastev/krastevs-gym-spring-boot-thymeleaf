fab40803b665ac43631ffe1289372d067edcf6f6
todorkrastev <krastevtodordinev@yahoo.com>
2024-03-14 13:37:16 +0100

    Initial Commit

---
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/KrastevsGymApplication.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrastevsGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrastevsGymApplication.class, args);
+    }
+
+}
---
+++ krastevs-gym/src/test/java/com/todorkrastev/krastevsgym/KrastevsGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrastevsGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
