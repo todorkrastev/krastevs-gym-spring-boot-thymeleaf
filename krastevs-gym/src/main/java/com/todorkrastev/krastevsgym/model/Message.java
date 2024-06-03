37b79683fdc774288942cf34e8827ab33104579d
todorkrastev <krastevtodordinev@yahoo.com>
2024-06-03 18:01:28 +0200

    Adding Message.java

---
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/model/Message.java
@@ -0,0 +1,69 @@
+package com.todorkrastev.krastevsgym.model;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+
+    @Column(name = "date_time", nullable = false)
+    private Instant dateTime;
+
+    @Column(name = "text_content", columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(optional = false)
+    private User author;
+
+    @ManyToOne(optional = false)
+    private User recipient;
+
+    public Message() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Instant dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+}
+
