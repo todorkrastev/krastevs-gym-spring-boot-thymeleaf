268394ad4f23104811ba0e6287923947a57797b5
todorkrastev <krastevtodordinev@yahoo.com>
2024-06-04 17:57:25 +0200

    Adding Config.java

---
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/config/Config.java
@@ -0,0 +1,14 @@
+package com.todorkrastev.krastevsgym.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
