ec3ad210253e2a31b99597d734683d3e56aec410
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-12 17:40:43 +0200

    Initial Commit

---
+++ krastevs-gym-exercises/src/main/java/com/todorkrastev/krastevsgymexercises/KrastevsGymExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgymexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrastevsGymExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrastevsGymExercisesApplication.class, args);
+    }
+
+}
---
+++ krastevs-gym-exercises/src/test/java/com/todorkrastev/krastevsgymexercises/KrastevsGymExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorkrastev.krastevsgymexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrastevsGymExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
