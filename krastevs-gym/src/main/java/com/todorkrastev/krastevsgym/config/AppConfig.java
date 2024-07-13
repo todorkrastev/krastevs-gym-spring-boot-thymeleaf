37ebfadef5b1b252bf99459ba2fa3de413651a7c
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-13 14:54:54 +0200

    Add i18n implementation

--- krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/config/AppConfig.java
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/config/AppConfig.java
@@ -1,25 +1,13 @@
 package com.todorkrastev.krastevsgym.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 public class AppConfig {
-
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:i18n/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-
-        return messageSource;
     }
 }
