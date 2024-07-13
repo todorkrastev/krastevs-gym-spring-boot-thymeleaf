1b079f55cad55504166d638fc814c6bf82573809
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-14 00:34:45 +0200

    Update i18n configuration

--- krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/KrastevsGymApplication.java
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/KrastevsGymApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KrastevsGymApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KrastevsGymApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KrastevsGymApplication.class, args);
+    }
 }
--- krastevs-gym/src/test/java/com/todorkrastev/krastevsgym/service/impl/UserServiceImplTest.java
+++ krastevs-gym/src/test/java/com/todorkrastev/krastevsgym/service/impl/UserServiceImplTest.java
@@ -1,22 +1,28 @@
 package com.todorkrastev.krastevsgym.service.impl;
 
 import com.todorkrastev.krastevsgym.model.dto.UserRegisterDTO;
+import com.todorkrastev.krastevsgym.model.entity.UserEntity;
 import com.todorkrastev.krastevsgym.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@Disabled
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
     private UserServiceImpl toTest;
+
+    @Captor
+    private ArgumentCaptor<UserEntity> userEntityCaptor;
 
     @Mock
     private UserRepository mockRepository;
@@ -42,6 +48,20 @@ class UserServiceImplTest {
                         .setLastName("Krastev")
                         .setPassword("top-secret");
 
+        when(mockPasswordEncoder.encode(userRegisterDTO.getPassword()))
+                .thenReturn(userRegisterDTO.getPassword() + userRegisterDTO.getPassword());
+
         toTest.registerUser(userRegisterDTO);
+
+        verify(mockRepository).save(userEntityCaptor.capture());
+
+        UserEntity actualSavedEntity = userEntityCaptor.getValue();
+
+        assertEquals(userRegisterDTO.getFirstName(), actualSavedEntity.getFirstName());
+        assertEquals(userRegisterDTO.getLastName(), actualSavedEntity.getLastName());
+        assertEquals(userRegisterDTO.getPassword() + userRegisterDTO.getPassword(), actualSavedEntity.getPassword());
+        assertEquals(userRegisterDTO.getEmail(), actualSavedEntity.getEmail());
+
+
     }
 }
