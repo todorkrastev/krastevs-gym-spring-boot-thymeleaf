9c76e51cad5c955ba2af6a21dd5bdccfd5c486e8
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-21 12:55:08 +0200

    Remove Activity entity and repository and improve UI/UX

--- krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/service/impl/ActivityServiceImpl.java
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/service/impl/ActivityServiceImpl.java
@@ -1,13 +1,9 @@
 package com.todorkrastev.krastevsgym.service.impl;
 
-import com.todorkrastev.krastevsgym.exception.ResourceNotFoundException;
 import com.todorkrastev.krastevsgym.model.dto.ActivityDTO;
-import com.todorkrastev.krastevsgym.model.entity.ActivityEntity;
-import com.todorkrastev.krastevsgym.repository.ActivityRepository;
 import com.todorkrastev.krastevsgym.service.ActivityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
@@ -21,13 +17,9 @@ public class ActivityServiceImpl implements ActivityService {
     private final Logger LOGGER = LoggerFactory.getLogger(ExerciseServiceImpl.class);
 
     private final RestClient activityRestClient;
-    private final ActivityRepository activityRepository;
-    private final ModelMapper modelMapper;
 
-    public ActivityServiceImpl(@Qualifier("activityRestClient")RestClient activityRestClient, ActivityRepository activityRepository, ModelMapper modelMapper) {
+    public ActivityServiceImpl(@Qualifier("activityRestClient") RestClient activityRestClient) {
         this.activityRestClient = activityRestClient;
-        this.activityRepository = activityRepository;
-        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -36,7 +28,7 @@ public class ActivityServiceImpl implements ActivityService {
 
         return activityRestClient
                 .get()
-                .uri("http://localhost:8081/activities")
+                .uri("http://localhost:8081/api/v1/activities/all")
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {
@@ -45,48 +37,20 @@ public class ActivityServiceImpl implements ActivityService {
 
     @Override
     public ActivityDTO getActivityById(Long activityId) {
-        ActivityEntity activity = this.activityRepository.findById(activityId).orElseThrow(() -> new ResourceNotFoundException("Activity", "id", activityId));
-
-        return this.modelMapper.map(activity, ActivityDTO.class);
-
-        /*
-        return this.activityRepository
-                .findById(activityId)
-                .map(activity ->
-                        this.modelMapper
-                                .map(activity, ActivityDTO.class));
-
-         */
+        return null;
     }
 
     @Override
     public ActivityDTO updateActivityById(Long activityId, ActivityDTO activityDTO) {
-        //TODO: Make a validation if the admin is doing the change
-
-        ActivityEntity activity = this.activityRepository.findById(activityId).orElseThrow(() -> new ResourceNotFoundException("Activity", "id", activityId));
-
-        activity.setTitle(activityDTO.getTitle());
-        activity.setDescription(activityDTO.getDescription());
-        activity.setImageURL(activityDTO.getImageURL());
-
-        ActivityEntity updatedActivity = this.activityRepository.save(activity);
-
-        return this.modelMapper.map(updatedActivity, ActivityDTO.class);
+        return null;
     }
 
     @Override
     public Long createActivity(ActivityDTO activityDTO) {
-        ActivityEntity activityToSave = this.modelMapper.map(activityDTO, ActivityEntity.class);
-
-        this.activityRepository.save(activityToSave);
-
-        return activityToSave.getId();
+        return null;
     }
 
     @Override
     public void deleteActivityById(Long activityId) {
-        ActivityEntity activity = this.activityRepository.findById(activityId).orElseThrow(() -> new ResourceNotFoundException("Activity", "id", activityId));
-
-        this.activityRepository.delete(activity);
     }
 }
