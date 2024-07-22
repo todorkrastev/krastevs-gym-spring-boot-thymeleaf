be02dcff033c2f0568a55e9b0ae5a95fab888323
todorkrastev <krastevtodordinev@yahoo.com>
2024-07-22 18:26:53 +0200

    Update Advice (AOP) for ExchangeRate

--- krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/web/CurrencyController.java
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/web/CurrencyController.java
@@ -3,6 +3,7 @@ package com.todorkrastev.krastevsgym.web;
 import com.todorkrastev.krastevsgym.exception.ApiObjectNotFoundException;
 import com.todorkrastev.krastevsgym.model.dto.ConversionResultDTO;
 import com.todorkrastev.krastevsgym.service.ExRateService;
+import com.todorkrastev.krastevsgym.web.aop.WarnWhenExecutionExceeds;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +18,18 @@ public class CurrencyController {
         this.exRateService = exRateService;
     }
 
+    @WarnWhenExecutionExceeds(timeInMilliseconds = 1000)
     @GetMapping("/api/convert")
     public ResponseEntity<ConversionResultDTO> convert(
             @RequestParam("from") String from,
             @RequestParam("to") String to,
             @RequestParam("amount") BigDecimal amount) {
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
         BigDecimal result = exRateService.convert(from, to, amount);
 
         return ResponseEntity.ok(new ConversionResultDTO(
