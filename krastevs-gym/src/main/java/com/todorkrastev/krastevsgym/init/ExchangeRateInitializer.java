35a163057167657b605c76cd0d8d83a4474a5bef
todorkrastev <todorkrastev@web.de>
2025-01-19 23:14:53 +0100

    Add Dockerfile and docker-compose.yml

--- krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/init/ExchangeRateInitializer.java
+++ krastevs-gym/src/main/java/com/todorkrastev/krastevsgym/init/ExchangeRateInitializer.java
@@ -2,11 +2,13 @@ package com.todorkrastev.krastevsgym.init;
 
 import com.todorkrastev.krastevsgym.service.ExRateService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("production")
+@ConditionalOnProperty(name = "forex.init-ex-rates", havingValue = "true")
 public class ExchangeRateInitializer implements CommandLineRunner {
     private final ExRateService exRateService;
 
